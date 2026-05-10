d7ad67dd605d0f661633582b313bf2947832ffb8
zhanglq <zhangliqiang365@outlook.com>
2026-05-10 16:57:04 +0800

    一：流式输出

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import { generateText } from 'ai'
+import { generateText, streamText } from 'ai'
 import { createOpenAI } from '@ai-sdk/openai';
 
 // 显示加载 .env.local
@@ -13,12 +13,23 @@ const client = createOpenAI({
 async function main() {
 
   try {
-    // 同步输出
-    const { text } = await generateText({
+    // 1. 同步输出
+    // const { text } = await generateText({
+    //   model: client.chat(process.env.DASHSCOPE_MODEL as string),
+    //   prompt: '用一句话介绍你自己'
+    // })
+    // console.log('测试：', text)
+
+    // 2. 流式输出
+    const result = await streamText({
       model: client.chat(process.env.DASHSCOPE_MODEL as string),
-      prompt: '用一句话介绍你自己'
+      prompt: '用一句话介绍你自己',
     })
-    console.log('测试：', text)
+    for await (const chunk of result.textStream) {
+      process.stdout.write(chunk);
+    }
+
+    console.log(); // 换行
   } catch (err: any) {
     console.error('message:', err?.message);
   }
